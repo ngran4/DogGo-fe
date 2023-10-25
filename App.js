8e09d0c1f84e242f707502783077b65049afbfe4
Nicholas Emmons <emmonsnicholas7@gmail.com>
2023-10-25 10:09:45 -0400

    SignUp bones made and WalkInfo started

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { UserSignUp } from './src/screens/signUp'; 
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <UserSignUp/>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +18,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
   },
 });
---
+++ src/screens/walkinfo.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {
+    Text,
+    StyleSheet,
+    Button,
+    View,
+    SafeAreaView
+} from "react-native"
+
+export const WalkCounter = () => {
+
+}
+
+return
