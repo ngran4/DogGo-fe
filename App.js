d4bf71ae97a564a864920133cded35532fa5cbf8
SaidNivek <kevin.vi.dias@gmail.com>
2023-10-24 21:58:18 -0400

    Welcome page lo-fi screen complete

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Welcome from './src/screens/Welcome';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={[styles.container, {backgroundColor: 'lightgrey'}]}>
+      <Welcome />
     </View>
   );
 }
@@ -13,7 +13,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ src/screens/Welcome.js
@@ -0,0 +1,71 @@
+import React from 'react'
+import { SafeAreaView, StyleSheet, Image, Text, TouchableOpacity } from 'react-native'
+
+
+const Welcome = () => {
+
+
+    const {container, button, title, info, buttonText} = styles
+
+    const goToLogin = () => {
+
+    }
+
+    const goToSignUp = () => {
+
+    }
+
+  return (
+    <SafeAreaView style={container}>
+        <Image source={require('../../assets/images/bring.png')}  style={{marginTop: 50, marginBottom:50}}/>
+        <Text style={title}>Welcome</Text>
+        <Text style={title}>to DogGo!</Text>
+        <Text style={[info, {marginTop: 50}]}>Get personalized walking reminders based on the weather</Text>
+        <Text style={info}> Stay on top of your dog's walking schedule</Text>
+        <TouchableOpacity style={[button, {marginTop: 100}]} onPress={goToLogin}>
+            <Text style={buttonText}>Log In</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={button} onPress={goToSignUp}>
+            <Text style={buttonText}>Sign Up</Text>
+        </TouchableOpacity>
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex:1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        
+        
+    },
+    title: {
+        fontSize: 40,
+    },
+    info: {
+        fontSize: 20,
+        textAlign: 'center',
+        padding: '3%'
+    },
+    button: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#DDDDDD',
+        padding: 10,
+        borderRadius: 20,
+        borderWidth: 1,
+        margin: 10, 
+        width: 310,
+        height: 58,
+    },
+    buttonText: {
+        fontSize: 20,
+    },
+    dog: {
+        marginTop: 15,
+        marginBottom: 15
+    }
+})
+
+export default Welcome
