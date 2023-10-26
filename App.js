25a5265d5dd9d2f339b0e7ee0ffb1ca8a3bbc228
SaidNivek <kevin.vi.dias@gmail.com>
2023-10-25 20:30:11 -0400

    Welcome page styled except for font

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet} from 'react-native';
 import { StateProvider } from './src/context/StateContext';
 import Welcome from './src/screens/Welcome';
 import LocationServices from './src/screens/LocationServices';
@@ -12,29 +12,26 @@ import { createStackNavigator } from '@react-navigation/stack'
 const Stack = createStackNavigator()
 
 export default function App() {
+  const navOptions = {
+    title: "",
+    headerStyle: {backgroundColor: "#F8F5E6"}
+  }
+
+  const noHeaderNavOptions = {
+    headerShown: false
+  }
+  
   return (
-    <StateProvider>
-        <NavigationContainer>
-          <Stack.Navigator >
-            <Stack.Screen name="Welcome" component={Welcome} options={{title:"", headerStyle: {backgroundColor: 'lightgrey'}}}/>
-            <Stack.Screen name="Location Services" component={LocationServices} options={{title:"", headerStyle: {backgroundColor: 'lightgrey'}}}/>
-            <Stack.Screen name="Sign Up" component={UserSignUp} options={{title:"", headerStyle: {backgroundColor: 'lightgrey'}}}/>
-            <Stack.Screen name="Add a Furry Friend" component={AddPet} options={{title:"", headerStyle: {backgroundColor: 'lightgrey'}}}/>
-            <Stack.Screen name="Walk Counter" component={WalkCounter} options={{title:"", headerStyle: {backgroundColor: 'lightgrey'}}}/>
-          </Stack.Navigator>
-        </NavigationContainer>
-    </StateProvider>
+      <StateProvider >
+          <NavigationContainer >
+            <Stack.Navigator >
+              <Stack.Screen name="Welcome" component={Welcome} options={noHeaderNavOptions}/>
+              <Stack.Screen name="Location Services" component={LocationServices} options={navOptions}/>
+              <Stack.Screen name="Sign Up" component={UserSignUp} options={navOptions}/>
+              <Stack.Screen name="Add a Furry Friend" component={AddPet} options={navOptions}/>
+              <Stack.Screen name="Walk Counter" component={WalkCounter} options={navOptions}/>
+            </Stack.Navigator>
+          </NavigationContainer>
+      </StateProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
-  },
-});
--- src/screens/Welcome.js
+++ src/screens/Welcome.js
@@ -1,76 +1,29 @@
 import React, { useContext } from 'react'
-import { SafeAreaView, StyleSheet, View, Text, TouchableOpacity } from 'react-native'
 import { StateContext } from '../context/StateContext'
+import { SafeAreaView, StyleSheet, View, Text, TouchableOpacity } from 'react-native'
 import DogGoIcon from '../../assets/images/DogGoIcon'
-import UserSignUp from './UserSignUp'
-import LocationServices from './LocationServices'
-
 
 const Welcome = ({navigation}) => {
-    const {container, button, title, info, buttonText} = styles
     const [styleContext] = useContext(StateContext)
-
-    const goToLogin = () => {
-
-    }
-
-    const goToSignUp = () => {
-
-    }
+    const {container, blueButton, greenButton, header, body, buttonText} = styleContext
 
   return (
     <SafeAreaView style={container}>
-        <View style={{height: 64, width: 64}}>
+        <View style={{height: 96, width: 100}}>
             <DogGoIcon />
         </View>
-        <Text style={styleContext.title}>Welcome</Text>
-        <Text style={styleContext.title}>to DogGo!</Text>
-        <Text style={[info, {marginTop: 50}]}>Get personalized walking reminders based on the weather</Text>
-        <Text style={info}> Stay on top of your dog's walking schedule</Text>
-        <TouchableOpacity style={button} onPress={() => navigation.navigate("Login")}>
+        <Text style={[header, {marginTop: 30}]}>Welcome</Text>
+        <Text style={header}>to DogGo!</Text>
+        <Text style={[body, {marginTop: 40, textAlign: 'center', padding: '3%'}]}>Get personalized walking reminders based on your local weather</Text>
+        <Text style={[body, {textAlign: 'center', padding: '3%'}]}> Stay on top of your dog's walking schedule</Text>
+        <TouchableOpacity style={[greenButton, {marginTop: 78}]} onPress={() => navigation.navigate("Login")}>
             <Text style={buttonText}>Log In</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={button} onPress={() => navigation.navigate("Sign Up")}>
+        <TouchableOpacity style={[blueButton, {marginTop: 17}]} onPress={() => navigation.navigate("Sign Up")}>
             <Text style={buttonText}>Sign Up</Text>
         </TouchableOpacity>
     </SafeAreaView>
   )
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex:1,
-        justifyContent: 'center',
-        alignItems: 'center',   
-    },
-    title: {
-        fontSize: 40,
-    },
-    info: {
-        fontSize: 20,
-        textAlign: 'center',
-        padding: '3%'
-    },
-    button: {
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#DDDDDD',
-        padding: 10,
-        borderRadius: 20,
-        borderWidth: 1,
-        margin: 10, 
-        width: 310,
-        height: 58,
-    },
-    buttonText: {
-        fontSize: 20,
-        maxWidth: 310,
-        maxHeight: 58,
-    },
-    dog: {
-        marginTop: 15,
-        marginBottom: 15
-    }
-})
 
 export default Welcome
