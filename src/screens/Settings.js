703dab804929e95055a3f689260c7c000a91973e
SaidNivek <kevin.vi.dias@gmail.com>
2023-10-27 15:01:08 -0400

    Add basic layout for other screens

--- src/screens/Settings.js
+++ src/screens/Settings.js
@@ -1,13 +1,15 @@
-import * as React from 'react';
+import React, { useContext } from 'react';
+import { StateContext } from '../context/StateContext'
 import { View, Text, SafeAreaView } from 'react-native'; 
 
-
-
 const Settings = () => {
+  const [stateContext] = useContext(StateContext)
+  const { container, header } = stateContext
+
   return (
-    <SafeAreaView>
+    <SafeAreaView style={container}>
       <View>
-        <Text>Settings</Text>
+        <Text style={header}>Settings</Text>
       </View>
     </SafeAreaView>
   )
