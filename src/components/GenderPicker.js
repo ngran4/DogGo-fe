73dc199e9a66471bb076dd0a04e115bde4146de0
Nicole Granville <nicgranville@gmail.com>
2024-01-26 16:50:05 -0800

    create gender dropdown component

---
+++ src/components/GenderPicker.js
@@ -0,0 +1,72 @@
+import React, { useContext, useState } from "react";
+import { StateContext } from "../context/StateContext";
+import DropDownPicker from "react-native-dropdown-picker";
+import { View, Dimensions } from "react-native";
+
+const screenWidth = Dimensions.get("window").width;
+
+const GenderPicker = () => {
+  const [stateContext] = useContext(StateContext);
+  const { gender, setGender, colors } = stateContext;
+  const [open, setOpen] = useState(false);
+  const [items, setItems] = useState([
+    { label: "Sex", value: "" },
+    { label: "Male", value: "M" },
+    { label: "Female", value: "F" },
+  ]);
+
+  return (
+    <View
+      style={{
+        borderBottomWidth: 2,
+        borderBottomColor: "black",
+        alignItems: "flex-start",
+        width: screenWidth * 0.28,
+        marginTop: 5,
+      }}
+    >
+      <DropDownPicker
+        open={open}
+        value={gender}
+        placeholder="Sex"
+        items={[
+          { label: "Male", value: "M" },
+          { label: "Female", value: "F" },
+        ]}
+        setOpen={setOpen}
+        setValue={setGender}
+        setItems={setItems}
+        style={{
+          backgroundColor: colors.background,
+          borderColor: colors.background,
+          width: screenWidth * 0.3,
+          paddingLeft: 0,
+          color: "grey",
+        }}
+        placeholderStyle={{
+          color: "grey",
+          fontSize: 20,
+          fontWeight: 600,
+          fontFamily: "OpenSans-Regular",
+          paddingLeft: 0,
+        }}
+        dropDownContainerStyle={{
+          backgroundColor: colors.background,
+          borderColor: colors.background,
+        }}
+        listItemLabelStyle={{
+          color: "grey",
+          fontSize: 15,
+          fontWeight: 600,
+          fontFamily: "OpenSans-Regular",
+        }}
+        selectedItemLabelStyle={{
+          color: "grey",
+          fontSize: 15,
+        }}
+      />
+    </View>
+  );
+};
+
+export default GenderPicker;
